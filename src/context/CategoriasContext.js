a884ed27ba880c599b9132a1431913486f6efb62
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-02-10 09:45:46 +0100

    llamado a la API

--- src/context/CategoriasContext.js
+++ src/context/CategoriasContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 //Crear el Context
 export const CategoriasContext = createContext();
@@ -7,12 +8,24 @@ export const CategoriasContext = createContext();
 const CategoriasProvider = (props) => {
 
     //crear el state del context
-    const [ hola, guardarHola ] = useState('hola desde state');
+    const [ categorias, guardarCategorias ] = useState([]);
+
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+            const categorias = await axios.get(url);
+
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategorias();
+    }, []);
 
     return(
         <CategoriasContext.Provider
             value={{
-                hola,
+                categorias
             }}
         >
             {props.children}
